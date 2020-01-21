30b2d9ded2ee66d6794deb8253affa5d2e8ee3bb
avisionh <a_vision@hotmail.co.uk>
2020-01-21 14:05:08 +0000

    feat: Add starter script for connecting and querying from BigQuery
    This is to lay the foundation to the rest of this project where we will transform the data in BigQuery, then pull it into R.

---
+++ main.R
@@ -0,0 +1,42 @@
+# -------- #
+## main.R ##
+# -------- #
+
+# ----------------------------------------------------------------
+# Desc: This is where the main code will live.
+# Credit: Please see functions.
+# Script Dependencies: None
+# Notes: Might break out into further scripts and an Rmarkdown file.
+# ----------------------------------------------------------------
+
+# import relevant packages
+vector_packages <- c(
+  "bigrquery", 
+  "DBI", 
+  "dplyr"
+)
+sapply(X = vector_packages, FUN = library, character.only = TRUE)
+
+# low-level API
+id_project <- "training-265812"
+sql_test <- "
+  SELECT visitorId
+    ,visitStartTime
+    ,date
+    ,totals
+  FROM `bigquery-public-data.google_analytics_sample.ga_sessions_20170801`
+"
+
+# Low-level API -----------------------------------------------------------
+
+query <- bq_project_query(x = id_project, query = sql_test)
+data <- bq_table_download(x = query, max_results = 15)
+
+
+# Using DBI ---------------------------------------------------------------
+
+conn_projecttraining <- dbConnect(drv = bigquery(),
+                                  project = "bigquery-public-data",
+                                  dataset = "google_analytics_sample",
+                                  billing = id_project)
+dbListTables(conn = conn_projecttraining)
